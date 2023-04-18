0a308c38d5cf80eb80d69fc1b72ee24ecac4e835
Matthew Brehm <royalmatt2309@gmail.com>
2023-04-17 21:11:49 -0700

    added all buttons required.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,5 +1,56 @@
-function helloWorld(){
+function getBigger(){
 
-    alert("Hello, world!");
+    var textarea = document.getElementById("textarea");
+    textarea.style.fontSize = "24px";
 
 }
+
+function changeStyle(){
+
+    var textarea = document.getElementById("textarea");
+    var fancyButtonId = document.getElementById("fancy");
+    var boringButtonId = document.getElementById("boring");
+
+    if(fancyButtonId.checked){
+
+        textarea.style.fontWeight = "bold";
+        textarea.style.textDecoration = "underline";
+        textarea.style.color = "blue";
+
+    }
+    if(boringButtonId.checked){
+
+        textarea.style.fontWeight = "normal";
+        textarea.style.textDecoration = "none";
+        textarea.style.color = "black";
+
+    }
+}
+
+function showAlert() {
+
+    alert("Changing font...");
+
+  }
+
+
+  function mooTime() {
+    var textarea = document.getElementById("textarea");
+    var text = textarea.value.toUpperCase();
+    var newText = "";
+  
+    var sentences = text.split(".");
+  
+    for (var i = 0; i < sentences.length - 1; i++) {
+
+        sentences[i] = sentences[i] + "-Moo";
+
+      //var words = sentences[i].split(" ");
+     // words[words.length - 1] = words[words.length - 1] + "-Moo";
+      //sentences[i] = words.join(" ");
+
+    }
+  
+    moodText = sentences.join(".");
+    textarea.value = moodText;
+  }
